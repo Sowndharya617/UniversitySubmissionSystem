5d6b466804fe63964de814844d11d924925a07da
Sowndharya617 <sowndharya617@gmail.com>
2025-08-07 14:57:49 +0530

    Added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Register from './pages/register'
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path = '/' element = {<Home/>}></Route>
+      <Route path = '/register' element = {<Register/>}></Route>
+
+    </Routes>
+    </BrowserRouter>
+
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,8 @@
+//server side codes!! server site app
+import express from  'express'
+
+const app = express()
+
+app.listen(3000,()=>{
+    console.log("Server is running")
+})
